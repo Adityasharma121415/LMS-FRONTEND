d952c6eb923a074f563fc511fda632ddf18d763e
Aditya Sharma <adityasharma121415@gmail.com>
2025-02-16 08:03:30 +0530

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        Loan Management System
+      </div>
+    </>
+  );
+}
+
+export default App;
